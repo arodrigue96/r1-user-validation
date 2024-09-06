48dee912c98ded3e52226a0e68a2ba3f73df3cbf
Aniol Rodriguez <arodrigue96@gmail.com>
2024-09-06 12:25:28 +0200

    arreglo comprobacion contraseña

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Datos del usuario
 const userName = "arodrigue96";
-const password = "QM%o*^Lw@C82*h9Ufea";
-const passwordCheck = "QM%o*^Lw@C82*h9Ufea";
+const password = "Qm%o*^lw@c82*h9ufea";
+const passwordCheck = "Qm%o*^lw@c82*h9ufea";
 const userAge = 28;
 
 // Expresiones regulares utilizadas
@@ -14,14 +14,12 @@ console.log(`El nombre de usuario es valido: ${userName.length >= 8}`);
 // Comprobación contraseña: NO SE COMO HACER ESTE PASO. He mirado en la página de Regex que pasaste y creo que son correctas pero no me funciona.
 console.log(
   "La contraseña es válida:",
-  containUpperCase.includes(password) && containNumber.includes(password)
+  containUpperCase.test(password) && containNumber.test(password)
 );
 
 // Comprobación contraseña igual que la primera
 console.log(
-  `La repetición de la contraseña es válida ${password === passwordCheck}`
+  `La repetición de la contraseña es válida: ${password === passwordCheck}`
 );
-console.log();
-
 // Comprobación si es mayor de edad
 console.log("Es mayor de edad:", userAge >= 18);
