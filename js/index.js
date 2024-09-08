3e6b104fbac78bca10d71d388cd106b138efe40f
Aniol Rodriguez <arodrigue96@gmail.com>
2024-09-08 19:21:09 +0200

    Refactor indetificadores

--- js/index.js
+++ js/index.js
@@ -9,20 +9,20 @@ const containUpperCase = /[A-Z]/;
 const containNumber = /[0-9]/;
 
 // Comprobación caracteres userName:
-const userCheck = userName.length >= 8;
+const isValidUsername = userName.length >= 8;
 
-// Comprobación contraseña upperCase:
-const passwordUpperLetterCheck =
+// Comprobación contraseña:
+const isValidPassword =
   containUpperCase.test(password) && containNumber.test(password);
 
 // Comprobación contraseña sea igual que la primera:
-const samePassword = password === repeatedPassword;
+const isSamePassword = password === repeatedPassword;
 
 // Comprobación si es mayor de edad:
 const isAdult = userAge >= 18;
 
 // Consola:
-console.log("El nombre de usuario es valido:", userCheck);
-console.log("La contraseña es válida:", passwordUpperLetterCheck);
-console.log("La repetición de la contraseña es válida:", samePassword);
+console.log("El nombre de usuario es valido:", isValidUsername);
+console.log("La contraseña es válida:", isValidPassword);
+console.log("La repetición de la contraseña es válida:", isSamePassword);
 console.log("Es mayor de edad:", isAdult);
